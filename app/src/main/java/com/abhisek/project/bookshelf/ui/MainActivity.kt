627954d7777f1b0cb79589ef7a02bf2f37b548a0
abhisek30 <abhisek.samntaray@gmail.com>
2024-09-26 13:32:44 +0530

    added application class

--- app/src/main/java/com/abhisek/project/bookshelf/ui/MainActivity.kt
+++ app/src/main/java/com/abhisek/project/bookshelf/ui/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.abhisek.project.bookshelf
+package com.abhisek.project.bookshelf.ui
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -12,36 +12,16 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.abhisek.project.bookshelf.ui.theme.BookShelfTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             BookShelfTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    BookShelfTheme {
-        Greeting("Android")
     }
 }
